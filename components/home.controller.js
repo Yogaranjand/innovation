b9f842a441f81ec2d183b208801eb77f91e862f0
Yogaranjan Das <yogaranjand@NGBLR-MAC10.local>
2019-05-02 09:33:20 +0530

    added bootstrap

--- components/home.controller.js
+++ components/home.controller.js
@@ -2,18 +2,36 @@
     angular.module('ngInnovation')
     .controller('homeController', HomeController);
 
-    HomeController.$inject = ['$scope'];
+    HomeController.$inject = ['$scope', '$http'];
 
-    function HomeController($scope) {
+    function HomeController($scope, $http) {
         $scope.test = "this is me!!!!";
 
         $scope.onUpload = function () {
             console.log("hello");
             let file = $scope.myFile;
             console.log("file ===", file);
-            var fileFormData = new FormData();
-            fileFormData.append('file', file);
+            var fileFormData = new FileReader();
+            fileFormData.readAsText(file);
+            //fileFormData.append('file', file);
             console.log(JSON.stringify(fileFormData));
+
+            // var request = {
+            //     method: 'POST',
+            //     url: '/api/fileupload/',
+            //     data: formdata,
+            //     headers: {
+            //         'Content-Type': undefined
+            //     }
+            // };
+
+            // // SEND THE FILES.
+            // $http(request)
+            //     .success(function (d) {
+            //         alert(d);
+            //     })
+            //     .error(function () {
+            //     });
         }
 
     }
